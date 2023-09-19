a859b16538ba6f122456fd15a928bf7c63cdaaac
Theara Ya <theara.ya@outlook.com>
2023-09-18 23:33:19 -0400

    cleanup

--- Utils.js
+++ Utils.js
@@ -1,17 +1,3 @@
 export function randomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
-
-export function useState(initialState) {
-    let state = initialState;
-    
-    function getState() {
-        return state;
-    }
-
-    function setState(newState) {
-        state = newState;
-    }
-
-    return [getState, setState];
-}
